63f178ee1ab966c9737e80bd9ce0612b430fe7fa
Matt Newsted <mnewsted@illinoislegalaid.org>
2026-01-16 11:51:29 -0600

    template title adjustment

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.UIAppealAfterHearing',
-      version='0.3.0',
+      version='0.3.1',
       description=('A docassemble extension.'),
       long_description='# docassemble.UIAppealAfterHearing\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
